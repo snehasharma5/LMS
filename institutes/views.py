2cc47c698a7b139df37b4e2db56efb32928b7783
Snehlata Sharma <snehlatasharma59@gmail.com>
2021-06-02 02:28:07 +0530

    institute_dashboard added

--- institutes/views.py
+++ institutes/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView
-from web_app.models import RegisterModel, Institute, Student
+from web_app.models import RegisterModel, Institute, Student, CourseModel
 from members.forms import InstituteRegisterForm, InstituteProfileForm
 
 
@@ -53,6 +53,7 @@ class InstituteDashboard(DetailView):
     template_name = 'institutes/institute_dashboard.html'
 
     def get_context_data(self, **kwargs):
-        students = Student.objects.all()
-        context = {'students': students}
+        students = Student.objects.all()[0:5]
+        courses = CourseModel.objects.all()[0:5]
+        context = {'students': students, 'courses':courses}
         return context
